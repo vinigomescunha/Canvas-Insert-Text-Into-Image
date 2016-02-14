2740fe07fa727b16686d22aacf77c84c4fd595bc
Vinicius Gomes <vinigomescunha@gmail.com>
2016-02-14 10:24:20 -0200

    organize

--- main.js
+++ main.js
@@ -17,34 +17,34 @@ for (var i in opts) {
 
 /* js manipule canvas from form :P */
 var ic = {
-	text: '',
-	canvas: '',
-	context: '',
-	dimensions: [],
+	text: '', 
+	canvas: '', 
+	context: '', 
+	dimensions: [], 
 	setFont: function($f) {
 		var link = document.createElement('link');
-		link.rel = 'stylesheet', link.id = $f.toLowerCase(), link.type = 'text/css',
+		link.rel = 'stylesheet', link.id = $f.toLowerCase(), link.type = 'text/css', 
 			link.href = 'http://fonts.googleapis.com/css?family=' + $f;
 		if (!document.getElementById($f.toLowerCase()))
 			document.getElementsByTagName('head')[0].appendChild(link);
-	},
+	}, 
 	handle: function(e) {
 		ic.setFont(document.getElementById('type').value.replace(' ', '+'));
 		ic.canvas = document.getElementById('imageCanvas');
 		ic.context = ic.canvas.getContext('2d');
 		ic.text = document.getElementById('texto').value;
-		var maxWidth = 0,
-			reader = new FileReader(),
+		var maxWidth = 0, 
+			reader = new FileReader(), 
 			img = new Image();
 		reader.onload = function(event) {
 			img.onload = function() {
-				var lineHeight = 30,
-				xv = document.getElementById('x').value,
-				xy = document.getElementById('y').value,
-				fs = document.getElementById('size').value,
-				ft = document.getElementById('type').value,
-				x = xv ? xv : 10,
-				y = xy ? xy : 20,
+				var lineHeight = 30, 
+				xv = document.getElementById('x').value, 
+				xy = document.getElementById('y').value, 
+				fs = document.getElementById('size').value, 
+				ft = document.getElementById('type').value, 
+				x = xv ? xv : 10, 
+				y = xy ? xy : 20, 
 				cfont = fs + 'px ' + ft;
 				ic.canvas.width = img.width;
 				ic.canvas.height = img.height;
@@ -61,26 +61,26 @@ var ic = {
 		if (!document.getElementById('d')) {
 			d = ic.create({e: 'button', params:{innerHTML: 'Download', id: 'd', className: 'form right', onclick: function() {ic.download();}}});
 			document.getElementById('submit').appendChild(d);
-			d = ic.create({e: 'button', params:{innerHTML: 'Invert', id: 'i', className: 'form right', onclick: function(){invert();}}});
+			d = ic.create({e: 'button', params:{innerHTML: 'Invert', id: 'i', className: 'form right', onclick: function() {invert();}}});
 			document.getElementById('submit').appendChild(d);
-			d = ic.create({e: 'button', params:{innerHTML: 'Grayscale', id: 'i', className: 'form right', onclick: function(){grayscale();}}});
+			d = ic.create({e: 'button', params:{innerHTML: 'Grayscale', id: 'i', className: 'form right', onclick: function() {grayscale();}}});
 			document.getElementById('submit').appendChild(d);
-			d = ic.create({e: 'button', params:{innerHTML: 'Opacity(Mark)', id: 'i', className: 'form right', onclick: function(){opacity();}}});
+			d = ic.create({e: 'button', params:{innerHTML: 'Opacity(Mark)', id: 'i', className: 'form right', onclick: function() {opacity();}}});
 			document.getElementById('submit').appendChild(d);
 		}
-	},
+	}, 
 	create: function (o) {
 		var d = document.createElement(o.e);
 		for(var i in o.params) d[i] = o.params[i];
 		return d;	
-	},
+	}, 
 	insertText: function(x, y, maxWidth, lineHeight) {
-		var w = ic.text.split(' '),
-			line = ' ';console.log(w)
+		var w = ic.text.split(' '), 
+			line = ' ';
 		for(var i=0; i < w.length; i++) {
 			var t  = line + w[i] + ' ';
 			tw = ic.context.measureText(t);
-			if(tw.width > maxWidth){
+			if(tw.width > maxWidth) {
 				ic.context.fillText(line, x, y);
 				line = w[i] + ' ';
 				y = parseInt(y) + lineHeight;
@@ -89,7 +89,7 @@ var ic = {
 			}
 			ic.context.fillText(line, x, y);
 		}
-	},
+	}, 
 	download: function() {
 		var l = document.createElement('a');
 		l.href = ic.canvas.toDataURL();
@@ -99,7 +99,7 @@ var ic = {
 	}
 };
 
-//ads click sample
+/* ads click sample */
 var canvas = document.getElementById('adversitment'), ctx = canvas.getContext('2d');
 canvas.onclick = function() {
 	ctx.fillStyle = '#000';
@@ -113,9 +113,8 @@ canvas.onclick = function() {
 	ctx.fillText(txt, parseInt(h), 85);
 }
 
-//ads sample
+/* ads sample */
 function simpleAdversitment() {
-	/* draw something */
 	ctx.fillStyle = '#EC6148';
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = '#ECE6A6';
@@ -129,7 +128,7 @@ function simpleAdversitment() {
 }
 simpleAdversitment();
 
-//watermark sample
+/* watermark sample */
 watermark = function() {
 	txt = 'This is a example of watermark';
     wcanvas = document.getElementById('watermarked_canvas');
@@ -152,11 +151,11 @@ watermark = function() {
     }
 }
 watermark()
-//invert color sample
-invert = function () {
-	var icanvas = document.getElementById('imageCanvas'),
+/* invert color sample */
+invert = function() {
+	var icanvas = document.getElementById('imageCanvas'), 
 	ictx = icanvas.getContext('2d');
-	idata = ictx.getImageData(0,0,icanvas.width, icanvas.height);
+	idata = ictx.getImageData(0, 0, icanvas.width, icanvas.height);
 	for (var i = 0; i < idata.data.length; i += 4) {
 		idata.data[i] = 255 - idata.data[i];     // red
 		idata.data[i + 1] = 255 - idata.data[i + 1]; // green
@@ -165,11 +164,11 @@ invert = function () {
 	ictx.putImageData(idata, 0, 0);
 	document.getElementById('imageCanvas').toDataURL('image/png');
 }
-//gray scale sample
-grayscale = function () {
-	var gcanvas = document.getElementById('imageCanvas'),
+/* gray scale sample */
+grayscale = function() {
+	var gcanvas = document.getElementById('imageCanvas'), 
 	gctx = gcanvas.getContext('2d');
-	gdata = gctx.getImageData(0,0,gcanvas.width, gcanvas.height);
+	gdata = gctx.getImageData(0, 0, gcanvas.width, gcanvas.height);
 	for (var i = 0; i < gdata.data.length; i += 4) {
 		var avg = (gdata.data[i] + gdata.data[i +1] + gdata.data[i +2]) / 3;
 		gdata.data[i] = avg;     // red
@@ -179,25 +178,24 @@ grayscale = function () {
 	gctx.putImageData(gdata, 0, 0);
 	document.getElementById('imageCanvas').toDataURL('image/png');
 }
-//opacity  sample image
+/* opacity  sample image */
 opacity = function() {
-	var ocanvas = document.getElementById("imageCanvas"),
+	var ocanvas = document.getElementById("imageCanvas"), 
 	octx = ocanvas.getContext("2d");
 	var img = new Image(); 
 	img.src = ocanvas.toDataURL('image/png');
-	var img1 = img;
 	if( img.complete ) {
-		octx.drawImage(img1, 0, 0);
+		octx.drawImage(img, 0, 0);
 	} else {
-		img.addEventListener('load', function() { octx.drawImage(img1, 0, 0); } );
+		img.addEventListener('load', function() { octx.drawImage(img, 0, 0); } );
 	}
-	var img = new Image();img.src = "mark.png",
-	img2 = img,
+	var img = new Image();
+	img.src = "mark.png", 
 	octx.globalAlpha = 0.25;
-	if( img2.complete ) {
-		octx.drawImage(img2, 0, 0);
+	if( img.complete ) {
+		octx.drawImage(img, 0, 0);
 	} else {
-		img.addEventListener('load', function() { octx.drawImage(img2, 0, 0); } );
+		img.addEventListener('load', function() { octx.drawImage(img, 0, 0); } );
 	}
 }
 
